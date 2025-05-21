22a0dfb036264f7158280310382e867f9985b338
Andres9884 <123489006+Andres9884@users.noreply.github.com>
2025-05-21 00:11:34 -0500

    Create COMMAND.cmd

---
+++ COMMAND.cmd
@@ -0,0 +1,13 @@
+@echo off
+set "input_folder=C:\Users\investigacion\Desktop\INFILEs\" :: Replace with you .in file location
+set "output_folder=C:\Users\investigacion\Desktop\INFILEs\" :: Replace with your simluation result destination
+set "mapdl_path=MAPDL.exe" :: You should have mapdl as an enviroment variable
+
+for %%f in ("%input_folder%\*.in") do (
+    "%mapdl_path%" -i "%%f" -o "E:\test.out" -b nolist -j "TRASH" -dir "%output_folder%" :: Create a .out file an replace its location
+    ren "%output_folder%\TRASH.rst" "%%~nf.rst"
+    del /f /q "%output_folder%\*TRASH*"
+    python E:\convergence_keeping.py %%~nf
+    echo Procesado exitoso: %%~nxf
+)
+
